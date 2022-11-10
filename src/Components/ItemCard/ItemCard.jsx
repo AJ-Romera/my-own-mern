87da19d8489122fbeed3fd1ad416f2cff42710cd
AJ Romera <ajromeraalfonso@gmail.com>
2022-11-10 14:26:11 +0100

    CHNG: Adds Price Container to ItemCard & Styles it

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -6,6 +6,10 @@ const ItemCard = ({ item }) => {
       <img src={item.imageUrl} alt="Item" className="item-card__img" />
       <p className="item-card__name">{item.name}</p>
       <p className="item-card__description">{item.description}</p>
+      <div className="item-card__buy-container">
+        <span className="item-card__price">{item.price}€</span>
+        <button className="item-card__buy-btn">Buy</button>
+      </div>
     </div>
   );
 };
