3e5bcf72362d725e21a56347e51fc39c3f0d2389
AJ Romera <ajromeraalfonso@gmail.com>
2022-11-10 20:34:57 +0100

    CHNG: Links each Item with its ItemDetails page

--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import "./itemCard.css";
 
 const ItemCard = ({ item }) => {
   return (
     <div className="item-card">
-      <img src={item.imageUrl} alt="Item" className="item-card__img" />
-      <p className="item-card__name">{item.name}</p>
+      <Link to={`/item/${item._id}`}>
+        <img src={item.imageUrl} alt="Item" className="item-card__img" />
+      </Link>
+      <p className="item-card__name">
+        <Link to={`/item/${item._id}`}>{item.name}</Link>
+      </p>
       <p className="item-card__description">{item.description}</p>
       <div className="item-card__buy-container">
         <span className="item-card__price">{item.price}€</span>
