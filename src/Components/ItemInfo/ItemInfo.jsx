7f1367000160d396acd7150b76dbea4c3f1e9fa7
AJ Romera <ajromeraalfonso@gmail.com>
2022-11-11 12:08:40 +0100

    CHNG: Makes & Styles the ItemInfo

--- src/Components/ItemInfo/ItemInfo.jsx
+++ src/Components/ItemInfo/ItemInfo.jsx
@@ -2,11 +2,16 @@ import "./itemInfo.css";
 
 const ItemInfo = ({ item }) => {
   return (
-    <div>
-      <div>{item.name}</div>
-      <div>{item.description}</div>
-      <div>{item.price}</div>
-      <img src={item.imageUrl} alt="item" />
+    <div className="item-info__container">
+      <div className="item-info__image-container">
+        <img src={item.imageUrl} alt="item" className="item-info__image" />
+      </div>
+      <div className="item-info__info-container">
+        <div className="item-info__name">{item.name}</div>
+        <div className="item-info__description">{item.description}</div>
+        <div className="item-info__price">{item.price}€</div>
+        <button className="item-info__button">Buy Now</button>
+      </div>
     </div>
   );
 };
